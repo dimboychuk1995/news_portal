049db930171346590b5ee89f039879c13a53d04d
Yurkiv Misha <jurkiw.misha@gmail.com>
2017-03-18 19:53:39 +0200

    fixed assets init

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.1'
+Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -10,4 +10,5 @@ Rails.application.config.assets.version = '1.1'
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( admin.css admin.js )
-Rails.application.config.assets.precompile += ['massive/*']
+Rails.application.config.assets.precompile += %w( style.css scripts.js )
+Rails.application.config.assets.precompile += %w( massive/* )
