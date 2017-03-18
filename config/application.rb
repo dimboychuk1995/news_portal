3d14efd1873f778489a9b386b8d2b84922de085e
Yurkiv Misha <jurkiw.misha@gmail.com>
2017-03-18 20:15:39 +0200

    assets fix

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module NewsPortal
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-
     config.generators do |g|
       g.test_framework nil
       g.stylesheets = false
