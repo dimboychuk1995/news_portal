0358b14bd72036c5fe556a02db3c67865f78f117
Yurkiv Misha <jurkiw.misha@gmail.com>
2017-03-18 17:59:41 +0200

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+set :tmp_dir
 
 ## Defaults:
 # set :scm,           :git
