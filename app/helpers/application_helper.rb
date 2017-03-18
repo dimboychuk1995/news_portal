fc4e2999af72403663790788f7ac425461c6648f
Yurkiv Misha <jurkiw.misha@gmail.com>
2017-03-18 23:31:44 +0200

    fixed active link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def show_time(time)
     time.strftime('%e %b %Y %H:%M:%S')
   end
+
+  def active_class(link_path)
+    request.url.include?(link_path) ? 'active' : ''
+  end
 end
